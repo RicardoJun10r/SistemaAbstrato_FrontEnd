9a5f8f3249e541dde26566efa8e5084304d9527c
RicardoJun10r <ricardojunior0110@gmail.com>
2023-04-21 01:28:57 -0300

    att

--- systemabstract/src/pages/Home/home.js
+++ systemabstract/src/pages/Home/home.js
@@ -1,11 +1,9 @@
 import React from "react";
-import VerticalNavBar from "../../components/NavBar/VerticalNavBar";
 import SideBar from "../../components/NavBar/SideBar";
 
 const Home = () => {
     return(
         <div>
-            {/* <VerticalNavBar/> */}
             <SideBar />
         </div>
     )
