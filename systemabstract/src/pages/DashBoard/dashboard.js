cb2c9f457b1c0406621e70faba49a952b8bc3be9
RicardoJun10r <ricardojunior0110@gmail.com>
2023-04-21 18:43:47 -0300

    adidionando os cards de balanco na dashboard

--- systemabstract/src/pages/DashBoard/dashboard.js
+++ systemabstract/src/pages/DashBoard/dashboard.js
@@ -1,12 +1,28 @@
 import React from "react";
-
+import CardBalanco from "../../components/Card/cardBalanco";
+import { dashBoardStyle } from "./dashBoardStyle";
+import { Grid } from "@mui/material";
 
 const DashBoard = () => {
     return(
-        <div>
+        <div style={dashBoardStyle}>
             <h1>
                 DashBoard
             </h1>
+            <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="flex-start"
+            > 
+                <CardBalanco />
+                <CardBalanco />
+                <CardBalanco />
+            </Grid>
+
+            <div>
+
+            </div>
         </div>
     )
 }
