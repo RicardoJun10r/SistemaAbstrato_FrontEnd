642d479e94b7cb0a0241f05e93205093bd94b2aa
RicardoJun10r <ricardojunior0110@gmail.com>
2023-04-21 18:14:36 -0300

    att

--- systemabstract/src/pages/Home/home.js
+++ systemabstract/src/pages/Home/home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import SideBar from "../../components/NavBar/sideBar";
+import { dashboardStyle } from "./style";
 
 const Home = () => {
     return(
-        <div>
-            <SideBar />
+        <div style={dashboardStyle}>
+            <header>
+                <SideBar />
+            </header>
+            <h1>DashBoard</h1>
         </div>
     )
 }
---
+++ systemabstract/src/pages/Home/style.js
@@ -0,0 +1,9 @@
+
+export const dashboardStyle = {
+    'backgroundColor': '#EEF9FF',
+    'fontFamily': 'roboto',
+    'display': 'flex',
+    'position': 'relative',
+    'height': '100vh',
+    'alignItems': 'center',
+}
