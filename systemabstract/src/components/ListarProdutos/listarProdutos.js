22bf6290fa7dd24145b7c109c39344fb23d25b5f
RicardoJun10r <ricardojunior0110@gmail.com>
2023-05-12 01:21:59 -0300

    refactor(produto): reestruturando a renderizacao da lista de produtos

--- systemabstract/src/components/ListarProdutos/listarProdutos.js
+++ systemabstract/src/components/ListarProdutos/listarProdutos.js
@@ -1,11 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import TabelaProdutos from "../Table/tabelaProdutos";
-import { Box, IconButton, ButtonGroup, Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import styled from "@emotion/styled";
-import ModalVer from "../Modal/modalVer";
-import PreviewIcon from '@mui/icons-material/Preview';
-import AddBoxIcon from '@mui/icons-material/AddBox';
-import DeleteIcon from '@mui/icons-material/Delete';
 
 const Item = styled(Box)(({ theme }) => ({
     backgroundColor: '#fff',
@@ -14,34 +10,12 @@ const Item = styled(Box)(({ theme }) => ({
 
 const ListarProdutos = (props) => {
 
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-
     return(
         <Box sx={{backgroundColor: '#FFFFFF'}}>
             <Grid item>
-                <Grid item>
-                    <Item>
-                        <ButtonGroup variant="outlined" aria-label="outlined button group">
-                            <IconButton onClick={handleOpen}>
-                                <PreviewIcon sx={{fontSize: '1.5em'}}/>
-                            </IconButton>
-                            <IconButton onClick={handleOpen}>
-                                <AddBoxIcon sx={{fontSize: '1.5em'}}/>
-                            </IconButton>
-                            <IconButton onClick={handleOpen}>
-                                <DeleteIcon sx={{fontSize: '1.5em'}}/>
-                            </IconButton>
-                        </ButtonGroup>
-                    </Item>
-                </Grid>
-                {open === false ? null : <ModalVer open={open} handleClose={handleClose} produto={props[0]}/>}
-                <Grid item>
-                    <Item>
-                        <TabelaProdutos props={props} />
-                    </Item>
-                </Grid>
+                <Item>
+                    <TabelaProdutos />
+                </Item>
             </Grid>
         </Box>
     )
