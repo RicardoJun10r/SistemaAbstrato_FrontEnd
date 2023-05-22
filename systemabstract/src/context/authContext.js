1041b1dd7d8edaa63e86ae16f70d60311c0545d2
RicardoJun10r <ricardojunior0110@gmail.com>
2023-05-22 12:36:27 -0300

    fix(authContext): now the authentication module is working

--- systemabstract/src/context/authContext.js
+++ systemabstract/src/context/authContext.js
@@ -1,25 +1,19 @@
 import React, { createContext, useState, useEffect } from "react";
-import { Logar, aplicacao } from "../api/api";
+import { aplicacao } from "../api/api";
 
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
 
-    const [ usuario, setUsuario ] = useState({});
+    const [ usuario, setUsuario ] = useState();
     
     const [ erros, setErros ] = useState();
 
     useEffect(() => {
         const usersStorage = localStorage.getItem("users_bd");
 
-        const user = usuario;
-    
         if (usersStorage) {
-          const hasUser = JSON.parse(usersStorage)?.filter(
-            (usu) => usu.email === user.login
-          );
-    
-          if (hasUser) setUsuario(hasUser[0]);
+          setUsuario(JSON.stringify(usersStorage));
         }
       }, []);
     
@@ -32,7 +26,7 @@ export const AuthProvider = ({ children }) => {
       }).catch(error => console.log(error.message))
 
 
-      if(user){
+      if(!!user){
         console.log("user: " + user.login)
 
         localStorage.setItem("users_bd", user)
@@ -40,25 +34,6 @@ export const AuthProvider = ({ children }) => {
         setUsuario(user)
         return "Logado!!!";
       }
-
-      // const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
-  
-      // const hasUser = usersStorage?.filter((user) => user.email === email);
-  
-      // if (hasUser?.length) {
-      //   if (hasUser[0].email === email && hasUser[0].password === password) {
-      //     const token = Math.random().toString(36).substring(2);
-      //     localStorage.setItem("user_token", JSON.stringify({ email, token }));
-      //     setUsuario({ email, password });
-      //     return;
-      //   } else {
-      //     setErros("E-mail ou senha incorretos")
-      //     return "E-mail ou senha incorretos";
-      //   }
-      // } else {
-      //   setErros("Usuário não cadastrado")
-      //   return "Usuário não cadastrado";
-      // }
     };
     
     const registrar = async (name, login, password) => {
@@ -71,32 +46,11 @@ export const AuthProvider = ({ children }) => {
       }).catch(error => console.log(error.message))
 
       return;
-
-      // const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
-  
-      // const hasUser = usersStorage?.filter((user) => user.email === email);
-  
-      // if (hasUser?.length) {
-      //   setErros("Já tem uma conta com esse E-mail")
-      //   return "Já tem uma conta com esse E-mail";
-      // }
-  
-      // let newUser;
-  
-      // if (usersStorage) {
-      //   newUser = [...usersStorage, { email, password }];
-      // } else {
-      //   newUser = [{ email, password }];
-      // }
-  
-      // localStorage.setItem("users_bd", JSON.stringify(newUser));
-  
-      // return;
     };
   
     const desautenticar = () => {
       setUsuario(null);
-      localStorage.removeItem("user_token");
+      localStorage.removeItem("users_bd");
     };
 
     return(
